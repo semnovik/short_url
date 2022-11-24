471df797eebc0be4076be337b022362c42554f31
semnovikov <semnovikov@ozon.ru>
2022-11-24 23:31:49 +0600

    added more tests

--- internal/app/service/shorter_test.go
+++ internal/app/service/shorter_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"short_url/internal/app/repository"
 	"testing"
@@ -29,4 +30,35 @@ func TestShorter_PostURL(t *testing.T) {
 		})
 	}
 
+}
+
+func TestShorter_GetURLByID(t *testing.T) {
+
+	type testCase struct {
+		name    string
+		send    string
+		want    string
+		wantErr bool
+		repo    *repository.Repository
+	}
+
+	tests := []testCase{
+		{name: "Simple positive", send: "1", want: "http://google.com", wantErr: false, repo: repository.NewRepository([]string{"http://google.com"})},
+		{name: "More than one url in repo", send: "2", want: "http://yandex.com", wantErr: false, repo: repository.NewRepository([]string{"http://google.com", "http://yandex.com"})},
+		{name: "Not found", send: "123", want: "", wantErr: true, repo: repository.NewRepository([]string{"http://google.com"})},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			service := Service{NewServer(test.repo)}
+			got, err := service.GetURLByID(test.send)
+
+			if test.wantErr == true {
+				require.NotNil(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			assert.Equal(t, test.want, got)
+		})
+	}
 }
