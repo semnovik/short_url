d84344e967e108701df640588d189a700c9377cf
semnovikov <semnovikov@ozon.ru>
2022-11-25 22:32:21 +0600

    done with tests

--- internal/app/service/shorter_test.go
+++ internal/app/service/shorter_test.go
@@ -1,11 +1,13 @@
 package service
 
 import (
+	"errors"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"short_url/internal/app/repository"
 	mock_repository "short_url/internal/app/repository/mocks"
+	"strconv"
 	"testing"
 )
 
@@ -58,27 +60,67 @@ func TestShorter_PostURL(t *testing.T) {
 }
 
 func TestShorter_GetURLByID(t *testing.T) {
+	type mockBehavior func(r *mock_repository.MockURLRepo, urlId int)
 
 	type testCase struct {
 		name    string
-		send    string
+		send    int
 		want    string
 		wantErr bool
-		repo    *repository.Repository
+		repo    mockBehavior
 	}
 
 	tests := []testCase{
-		{name: "Simple positive", send: "1", want: "http://google.com", wantErr: false, repo: repository.NewRepository([]string{"http://google.com"})},
-		{name: "More than one url in repo", send: "2", want: "http://yandex.com", wantErr: false, repo: repository.NewRepository([]string{"http://google.com", "http://yandex.com"})},
-		{name: "Not found", send: "123", want: "", wantErr: true, repo: repository.NewRepository([]string{"http://google.com"})},
-		{name: "symbols in id", send: "qwert", want: "", wantErr: true, repo: repository.NewRepository([]string{"http://google.com"})},
-		{name: "id isn't set", send: "", want: "", wantErr: true, repo: repository.NewRepository([]string{"http://google.com"})},
+		{
+			name:    "Simple positive",
+			send:    1,
+			want:    "http://google.com",
+			wantErr: false,
+			repo: func(r *mock_repository.MockURLRepo, urlId int) {
+				r.EXPECT().Get(urlId).Return("http://google.com", nil)
+			},
+		},
+		{
+			name:    "More than one url in repo",
+			send:    2,
+			want:    "http://yandex.com",
+			wantErr: false,
+			repo: func(r *mock_repository.MockURLRepo, urlId int) {
+				r.EXPECT().Get(urlId).Return("http://yandex.com", nil)
+			},
+		},
+		{
+			name:    "Not found",
+			send:    123,
+			want:    "",
+			wantErr: true,
+			repo: func(r *mock_repository.MockURLRepo, urlId int) {
+				r.EXPECT().Get(urlId).Return("", errors.New("url with that id is not found"))
+			},
+		},
+		{
+			name:    "id is 0",
+			send:    0,
+			want:    "",
+			wantErr: true,
+			repo: func(r *mock_repository.MockURLRepo, urlId int) {
+				r.EXPECT().Get(urlId).Return("", errors.New("url with that id is not found"))
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			service := Service{NewServer(test.repo)}
-			got, err := service.GetURLByID(test.send)
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			urlRepo := mock_repository.NewMockURLRepo(c)
+			test.repo(urlRepo, test.send)
+
+			repo := &repository.Repository{URLRepo: urlRepo}
+
+			service := &Service{NewShorter(repo)}
+			got, err := service.GetURLByID(strconv.Itoa(test.send))
 
 			if test.wantErr == true {
 				require.Error(t, err)
