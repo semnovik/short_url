d507e18fb832b64f958359d361dab483348b0585
semnovikov <semnovikov@ozon.ru>
2022-11-26 15:36:22 +0600

    go vet fixes

--- internal/app/repository/url_repo_test.go
+++ internal/app/repository/url_repo_test.go
@@ -9,22 +9,22 @@ func TestRepoURL_Add(t *testing.T) {
 
 	type testCase struct {
 		name    string
-		sendUrl string
-		wantId  int
+		sendURL string
+		wantID  int
 		repo    URLRepo
 	}
 
 	tests := []testCase{
-		{name: "add to empty", sendUrl: "http://google.com", wantId: 1, repo: NewRepository([]string{})},
-		{name: "add when some exists", sendUrl: "http://google.com", wantId: 2, repo: NewRepository([]string{"some"})},
-		{name: "add empty string", sendUrl: "", wantId: 1, repo: NewRepository([]string{})},
+		{name: "add to empty", sendURL: "http://google.com", wantID: 1, repo: NewRepository([]string{})},
+		{name: "add when some exists", sendURL: "http://google.com", wantID: 2, repo: NewRepository([]string{"some"})},
+		{name: "add empty string", sendURL: "", wantID: 1, repo: NewRepository([]string{})},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			gotId := test.repo.Add(test.sendUrl)
+			gotID := test.repo.Add(test.sendURL)
 
-			require.Equal(t, test.wantId, gotId)
+			require.Equal(t, test.wantID, gotID)
 		})
 	}
 
@@ -34,27 +34,27 @@ func TestRepoURL_Get(t *testing.T) {
 
 	type testCase struct {
 		name    string
-		sendId  int
-		wantUrl string
+		sendID  int
+		wantURL string
 		wantErr bool
 		repo    URLRepo
 	}
 
 	tests := []testCase{
-		{name: "get existing url", sendId: 1, wantUrl: "http://google.com", wantErr: false, repo: NewRepository([]string{"http://google.com"})},
-		{name: "get url, which doesn't exist ", sendId: 2, wantUrl: "", wantErr: true, repo: NewRepository([]string{"http://google.com"})},
-		{name: "get second existing url", sendId: 2, wantUrl: "http://google.com", wantErr: false, repo: NewRepository([]string{"ya.ru", "http://google.com"})},
+		{name: "get existing url", sendID: 1, wantURL: "http://google.com", wantErr: false, repo: NewRepository([]string{"http://google.com"})},
+		{name: "get url, which doesn't exist ", sendID: 2, wantURL: "", wantErr: true, repo: NewRepository([]string{"http://google.com"})},
+		{name: "get second existing url", sendID: 2, wantURL: "http://google.com", wantErr: false, repo: NewRepository([]string{"ya.ru", "http://google.com"})},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			gotUrl, err := test.repo.Get(test.sendId)
+			gotUrl, err := test.repo.Get(test.sendID)
 			if test.wantErr {
 				require.Error(t, err)
 			} else {
 				require.NoError(t, err)
 			}
-			require.Equal(t, test.wantUrl, gotUrl)
+			require.Equal(t, test.wantURL, gotUrl)
 		})
 	}
 }
