87f631f9ed1372960272ad1c0a6ea6eb61f05955
semnovikov <semnovikov@ozon.ru>
2022-12-15 21:19:18 +0300

    [increment-5] test config

--- configs/config.go
+++ configs/config.go
@@ -1,23 +1,23 @@
 package configs
 
-//import (
-//	"github.com/caarlos0/env/v6"
-//	"log"
-//)
-//
-//var Config = InitConfig()
-//
-//type Conf struct {
-//	ServerAddress string `env:"SERVER_ADDRESS"`
-//	//BaseURL       string `env:"BASE_URL,required"`
-//}
-//
-//func InitConfig() Conf {
-//	var cfg Conf
-//
-//	err := env.Parse(&cfg)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	return cfg
-//}
+import (
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
+var Config = InitConfig()
+
+type Conf struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	//BaseURL       string `env:"BASE_URL,required"`
+}
+
+func InitConfig() Conf {
+	var cfg Conf
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cfg
+}
