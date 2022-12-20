16f6c4452caebaf8db179e4043127253f887cd32
semnovikov <semnovikov@ozon.ru>
2022-12-20 18:13:18 +0300

    [increment-8] replaced init func for flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"os"
 	"short_url/configs"
 	"short_url/internal/repository"
 	"short_url/internal/server"
 )
 
 func main() {
-	initFlags()
+	configs.InitFlags()
 	repo, err := repository.NewURLRepository()
 	if err != nil {
 		log.Fatal(err)
@@ -22,18 +20,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func initFlags() {
-	if _, exist := os.LookupEnv("SERVER_ADDRESS"); !exist {
-		flag.StringVar(&configs.Config.ServerAddress, "a", ":8080", "address of the server")
-	}
-	if _, exist := os.LookupEnv("BASE_URL"); !exist {
-		flag.StringVar(&configs.Config.BaseURL, "b", "http://localhost:8080", "base URL of the server")
-	}
-	if _, exist := os.LookupEnv("FILE_STORAGE_PATH"); !exist {
-		flag.StringVar(&configs.Config.FileStoragePath, "f", "./internal/repository/file_with_urls", "path to file with urls")
-	}
-
-	flag.Parse()
 }
